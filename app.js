f387f8ed866b3f43b4121a609d164ac3ddb905cb
Edgar779 <cherqezyancherqezyan97@gmail.com>
2020-11-23 20:38:09 +0400

    Commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 10
 
 
 app.get('/', (req,res)=>{
-    res.redirect('http://localhost:3000/');
+    res.redirect('https://fintechtest555.herokuapp.com');
 })
 
 // app.get('/', (req, res) => {
